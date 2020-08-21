81356b0d721b51919277f5ee48a4fefb34a66b75
Josephshitandi <jsphshtnd@gmail.com>
2020-08-21 22:37:15 +0300

    added two arrays with male and female name

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var akanMaleNames = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+  ];
+  var akanFemaleNames = [
+    "Akosua",
+    "Adwoa",
+    "Abena",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama",
+  ];
