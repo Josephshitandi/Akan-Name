36d0aaa138ffd4a16754447c76037c0c50530876
Josephshitandi <jsphshtnd@gmail.com>
2020-08-22 09:15:37 +0300

    added getElementById functions that fetch data from html

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,9 @@
-var akanMaleNames = [
-    "Kwasi",
-    "Kwadwo",
-    "Kwabena",
-    "Kwaku",
-    "Yaw",
-    "Kofi",
-    "Kwame",
-  ];
-  var akanFemaleNames = [
-    "Akosua",
-    "Adwoa",
-    "Abena",
-    "Akua",
-    "Yaa",
-    "Afua",
-    "Ama",
-  ];
+var akanMaleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame",];
+  var akanFemaleNames = ["Akosua","Adwoa","Abena","Akua","Yaa","Afua","Ama",];
+  function akanName(){
+      var gender = document.getElementById("gender").value;
+      var ydate = document.getElementById("ydate").value;
+      alert(ydate);
+  }
+ 
+  
